b143cd27b1da0ddf013b7943171116a7891ccf2d
franciscopmbraz <ricardo.braz918@gmail.com>
2026-05-18 20:58:21 +0100

    inicio

---
+++ script.js
@@ -0,0 +1,9 @@
+let botaoSignUp = document.querySelector('.btn-signup');
+botaoSignUp.addEventListener('click', function() {
+        window.location.href = 'signup.html';
+    });
+
+let botaoLogin = document.querySelector('.btn-login');
+botaoLogin.addEventListener('click', function() {
+        window.location.href = 'landing.html';
+    });
