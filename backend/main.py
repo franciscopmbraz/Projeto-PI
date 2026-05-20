a2d7918807c7673493dc40a7484815d9a54c7a77
franciscopmbraz <ricardo.braz918@gmail.com>
2026-05-20 14:48:52 +0100

    ..

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, Field
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy.orm import declarative_base, sessionmaker, Session
 
-app = FastAPI(title="SVE - API Básica")
-
-@app.get("/")
-def raiz():
-    return {"status": "Sucesso", "mensagem": "O backend do teu SVE está a rodar no Docker!"}
