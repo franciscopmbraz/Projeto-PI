1281ac6477688744aec7bda0de88fdb438fe31c7
franciscopmbraz <ricardo.braz918@gmail.com>
2026-05-22 00:13:38 +0100

    singup feito e terminado

--- frontend/script.js
+++ frontend/script.js
@@ -13,6 +13,22 @@ if (formSignup) {
         let numeroAluno = document.querySelector('#numero_aluno').value;
         let anoLetivo = document.querySelector('#ano').value;
         let senhaAluno = document.querySelector('#senha').value;
+        
+
+        if (numeroAluno.length < 3) {
+            alert("O Número de Aluno tem de ter pelo menos 3 caracteres.");
+            return; // O 'return' cancela a operação e não deixa o código continuar!
+        }
+        
+        if (anoLetivo.length < 1) {
+            alert("O Ano Escolar não pode estar vazio.");
+            return;
+        }
+        
+        if (senhaAluno.length < 4) {
+            alert("A Palavra-passe tem de ter pelo menos 4 caracteres.");
+            return;
+        }
 
         try {
             // Envia os dados para a tua API (FastAPI) no Docker
@@ -32,7 +48,7 @@ if (formSignup) {
 
             // Verifica a resposta da API
             if (resposta.ok) {
-                alert("🎉 " + dados.mensagem); // Mensagem de sucesso!
+                alert("Aluno registado com sucesso! Agora podes fazer login.");
                 window.location.href = 'login.html'; // Manda o aluno para o Login
             } else {
                 // Se der erro mostra o alerta
@@ -47,12 +63,13 @@ if (formSignup) {
 }
 
 
+let formLogin = document.querySelector('#formLogin');
 
-
-
-
-
-let botaoLogin = document.querySelector('.btn-login');
-botaoLogin.addEventListener('click', function() {
+// O redirecionamento solto só vai existir se o formLogin estiver no ecrã!
+if (formLogin) {
+    let botaoLogin = document.querySelector('.btn-login');
+    botaoLogin.addEventListener('click', function(evento) {
+        evento.preventDefault();
         window.location.href = 'landing.html';
     });
+}
